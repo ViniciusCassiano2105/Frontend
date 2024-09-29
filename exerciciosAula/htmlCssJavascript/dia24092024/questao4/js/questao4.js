243892729f61965e678e4277761f4bbf9dbf4441
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-29 17:52:51 -0300

    Questão4

--- exerciciosAula/htmlCssJavascript/dia24092024/questao4/js/questao4.js
+++ exerciciosAula/htmlCssJavascript/dia24092024/questao4/js/questao4.js
@@ -1,13 +1,3 @@
-let curtidas = 0;
-
-document.getElementById("botaoCurtir").onclick = function() {
-    curtidas++;
-    document.getElementById("curtidas").innerText = curtidas;
-}
-
-document.getElementById("botaoDescurtir").onclick = function() {
-    if (curtidas > 0) {
-        curtidas--;
-    }
-    document.getElementById("curtidas").innerText = curtidas;
+document.getElementById("botaoMudaTexto").onclick = function() {
+    document.getElementById("botaoMudaTexto").innerText = "Obrigado por clicar!";
 }
